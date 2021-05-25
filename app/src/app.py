18a39fe6317bcac922b25c89e42f534db532a03b
Satoru Tanaka <ittimfn@gmail.com>
2021-05-25 10:33:52 +0900

    init

--- app/src/app.py
+++ app/src/app.py
@@ -5,5 +5,5 @@ import MeCab
 DIC_PATH = ' -d /usr/local/mecab/lib/mecab/dic/mecab-ipadic-neologd'
 
 if __name__ == '__main__':
-  sample_text = 'すもももももももものうち'
+  sample_text = '国境の長いトンネルを抜けると雪国であった。'
   print(MeCab.Tagger(DIC_PATH).parse(sample_text))
